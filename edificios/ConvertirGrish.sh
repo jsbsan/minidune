86a3e43b552b406a17ea90ef84255c20c2c8b265
jsbsan <jusabejusabe@gmail.com>
2022-04-24 18:59:36 +0200

    first commit

---
+++ edificios/ConvertirGrish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+convert ./VehiculoCosechadoraVerde.png -colorspace gray ./VehiculoCosechadoraVerdeGRIS.png
+convert ./VehiculoCosechadoraRojo.png -colorspace gray ./VehiculoCosechadoraRojoGRIS.png
+convert ./VehiculoCosechadoraAzul.png -colorspace gray ./VehiculoCosechadoraAzulGRIS.png
+convert ./TorretaDefensa.png -colorspace gray ./TorretaDefensaGRIS.png
+convert ./silo.png -colorspace gray ./siloGRIS.png
+convert ./Radar.png -colorspace gray ./RadarGRIS.png
+convert ./PlantaFabricaVehiculoLigeros.png -colorspace gray ./PlantaFabricaVehiculoLigerosGRIS.png
+convert ./PlantaFabricaPrincipal.png -colorspace gray ./PlantaFabricaPrincipalGRIS.png
+convert ./PlantaEolica.png -colorspace gray ./PlantaEolicaGRIS.png
+convert ./FactoriaAltaTecnologia.png -colorspace gray ./FactoriaAltaTecnologiaGRIS.png
+convert ./FabricaVehiculosPesados.png -colorspace gray ./FabricaVehiculosPesadosGRIS.png
+convert ./FabricaVehiculosLigeros.png -colorspace gray ./FabricaVehiculosLigerosGRIS.png
+convert ./FabricaReparacion.png -colorspace gray ./FabricaReparacionGRIS.png
+convert ./FabricaRefineriaEspecie.png -colorspace gray ./FabricaRefineriaEspecieGRIS.png
+convert ./FabricaMolinoEolico.png -colorspace gray ./FabricaMolinoEolicoGRIS.png
+convert ./FabricaCuartelPersonal.png -colorspace gray ./FabricaCuartelPersonalGRIS.png
+convert ./EnPlantaFabricaCosechadora.png -colorspace gray ./EnPlantaFabricaCosechadoraGRIS.png
+
+
